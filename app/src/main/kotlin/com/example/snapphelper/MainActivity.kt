1d5a9c28f48c2899541dada3fe2811f95ef36b11
parsagames <p83131057@gmail.com>
2026-09-25 00:41:47 +0330

    Update MainActivity.kt

--- app/src/main/kotlin/com/example/snapphelper/MainActivity.kt
+++ app/src/main/kotlin/com/example/snapphelper/MainActivity.kt
@@ -105,7 +105,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onRequestPermissionsResult(
         requestCode: Int,
-        permissions: Array<out String>,
+        permissions: Array<String>,
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
