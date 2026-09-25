759a3e0bd51c21ccbee590bf35960901815a4bec
parsagames <p83131057@gmail.com>
2026-09-25 14:15:55 +0330

    Delete app directory

--- app/src/main/kotlin/com/example/snapphelper/OrderColor.kt
+++
@@ -1,9 +0,0 @@
-package com.example.snapphelper
-
-enum class OrderColor(val persian: String) {
-    GREEN("سبز"),
-    PINK("صورتی"),
-    BLUE("آبی"),
-    CYAN("فیروزه‌ای"),
-    UNKNOWN("نامشخص")
-}
