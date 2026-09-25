9a9af61f7f97c2faf88aa289315f84ffc47318d5
parsagames <p83131057@gmail.com>
2026-09-25 20:27:41 +0330

    Update OrderColor.kt

--- app/src/main/kotlin/com/example/snapphelper/OrderColor.kt
+++ app/src/main/kotlin/com/example/snapphelper/OrderColor.kt
@@ -1,9 +1,9 @@
 package com.example.snapphelper
 
-enum class OrderColor(val persian: String) {
-    GREEN("سبز"),
-    PINK("صورتی"),
-    BLUE("آبی"),
-    CYAN("فیروزه‌ای"),
-    UNKNOWN("نامشخص")
+enum class OrderColor(val persian: String, val english: String) {
+    GREEN("سبز", "green"),
+    PINK("صورتی", "pink"),
+    BLUE("آبی", "blue"),
+    CYAN("فیروزه‌ای", "cyan"),
+    UNKNOWN("نامشخص", "unknown")
 }
